e24c25a55912408bcd44248353b1a332e3451ab1
Balázs Sipos <bsipos@precognox.com>
2019-11-22 16:09:51 +0100

    Improvements

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
-echo 'source .dotfiles/aliases' >> ~/.bashrc
-echo 'source .dotfiles/aliases' >> ~/.zshrc
+echo 'source ~/.dotfiles/aliases' >> ~/.bashrc
+echo 'source ~/.dotfiles/aliases' >> ~/.zshrc
+
+source ~/.zshrc
+source ~/.bashrc
